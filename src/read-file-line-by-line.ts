0782f748641e7014464eb83f4ecc5152a238ba1e
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2021-03-29 18:41:33 +0000

    feat: add readFileLineByLine

---
+++ src/read-file-line-by-line.ts
@@ -0,0 +1,11 @@
+import * as fs from 'fs'
+import * as readline from 'readline'
+
+export function readFileLineByLine(filename: string, encoding: BufferEncoding = 'utf-8'): AsyncIterable<string> {
+  const fileStream = fs.createReadStream(filename, { encoding })
+
+  return readline.createInterface({
+    input: fileStream
+  , crlfDelay: Infinity
+  })
+}
