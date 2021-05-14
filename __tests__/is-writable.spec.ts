a04d22f43c4eb38e886e079392e6b03fb7c26b56
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2021-05-14 05:35:57 +0000

    ci: try to fix test cases

--- __tests__/is-writable.spec.ts
+++ __tests__/is-writable.spec.ts
@@ -1,6 +1,20 @@
 import { isWritable } from '@src/is-writable'
-import { fixture } from '@test/utils'
+import { fixture, temp } from '@test/utils'
+import { ensureDir } from '@src/ensure-dir'
+import { emptyDir } from 'fs-extra'
+import { remove } from '@src/remove'
+import { promises as fs } from 'fs'
+import { ensureFile } from '@src/ensure-file'
 import '@blackglory/jest-matchers'
+
+// If we set an unwritable fixture,
+// then ci will failed because git does not keep the file mode.
+
+beforeEach(async () => {
+  await ensureDir(temp('.'))
+  await emptyDir(temp('.'))
+})
+afterEach(() => remove(temp('.')))
 
 describe('isWritable(path: string): Promise<boolean>', () => {
   describe('target is writable', () => {
@@ -15,7 +29,11 @@ describe('isWritable(path: string): Promise<boolean>', () => {
 
   describe('target is unwritable', () => {
     it('return false', async () => {
-      const result = isWritable(fixture('unwritable'))
+      const fixture = temp('unwritable')
+      await ensureFile(fixture)
+      await fs.chmod(fixture, 0o555)
+
+      const result = isWritable(fixture)
       const proResult = await result
 
       expect(result).toBePromise()
