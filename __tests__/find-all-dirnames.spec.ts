d1e8832d8988438d6bd09ac4f28219c80920bb8b
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2021-05-14 04:43:51 +0000

    test: findAllDirnames, findAllFilenames

---
+++ __tests__/find-all-dirnames.spec.ts
@@ -0,0 +1,42 @@
+import { findAllDirnames } from '@src/find-all-dirnames'
+import { fixture } from '@test/utils'
+import { toArrayAsync } from 'iterable-operator'
+import '@blackglory/jest-matchers'
+import 'jest-extended'
+
+describe(`
+  findAllDirnames(
+    dir: string
+  , predicate: (dirname: string) => boolean
+  ): AsyncIterable<string>
+`, () => {
+  it('return absolute paths', async () => {
+    const fn = jest.fn().mockReturnValue(true)
+    const dirname = fixture('nested')
+
+    const result = findAllDirnames(dirname, fn)
+    const proResult = await toArrayAsync(result)
+
+    expect(result).toBeAsyncIterable()
+    expect(proResult).toIncludeSameMembers([
+      fixture('nested/directory')
+    , fixture('nested/directory/deep-directory')
+    ])
+    expect(fn).toBeCalledTimes(2)
+    expect(fn).nthCalledWith(1, fixture('nested/directory'))
+    expect(fn).nthCalledWith(2, fixture('nested/directory/deep-directory'))
+  })
+
+  test('predicate returns false', async () => {
+    const fn = jest.fn().mockReturnValue(false)
+    const dirname = fixture('nested')
+
+    const result = findAllDirnames(dirname, fn)
+    const proResult = await toArrayAsync(result)
+
+    expect(result).toBeAsyncIterable()
+    expect(proResult).toStrictEqual([])
+    expect(fn).toBeCalledTimes(1)
+    expect(fn).nthCalledWith(1, fixture('nested/directory'))
+  })
+})
---
+++ __tests__/find-all-filenames.spec.ts
@@ -0,0 +1,42 @@
+import { findAllFilenames } from '@src/find-all-filenames'
+import { fixture } from '@test/utils'
+import { toArrayAsync } from 'iterable-operator'
+import '@blackglory/jest-matchers'
+import 'jest-extended'
+
+describe(`
+  findAllFilenames(
+    dir: string
+  , predicate: (dirname: string) => boolean
+  ): AsyncIterable<string>
+`, () => {
+  it('return absolute paths', async () => {
+    const fn = jest.fn().mockReturnValue(true)
+
+    const result = findAllFilenames(fixture('nested'), fn)
+    const proResult = await toArrayAsync(result)
+
+    expect(result).toBeAsyncIterable()
+    expect(proResult).toIncludeSameMembers([
+      fixture('nested/file')
+    , fixture('nested/directory/deep-file')
+    ])
+    expect(fn).toBeCalledTimes(2)
+    expect(fn).nthCalledWith(1, fixture('nested/directory'))
+    expect(fn).nthCalledWith(2, fixture('nested/directory/deep-directory'))
+  })
+
+  test('predicate returns false', async () => {
+    const fn = jest.fn().mockReturnValue(false)
+
+    const result = findAllFilenames(fixture('nested'), fn)
+    const proResult = await toArrayAsync(result)
+
+    expect(result).toBeAsyncIterable()
+    expect(proResult).toIncludeSameMembers([
+      fixture('nested/file')
+    ])
+    expect(fn).toBeCalledTimes(1)
+    expect(fn).nthCalledWith(1, fixture('nested/directory'))
+  })
+})
