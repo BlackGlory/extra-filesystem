16943c3d88a7e3de0d61783dedd128c87ff98d9b
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2021-09-10 14:39:26 +0000

    chore: add comments

--- src/is-sub-path-of.ts
+++ src/is-sub-path-of.ts
@@ -1,5 +1,6 @@
 import * as path from 'path'
 
+// Source: https://stackoverflow.com/a/45242825/5462167
 export function isSubPathOf(subject: string, object: string): boolean {
   const relative = path.relative(object, subject)
   return relative !== ''
