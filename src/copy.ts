0935acf4ee8583e68dfc5d0f40b6d54422c5c7c3
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2026-02-28 10:56:38 +0000

    docs: update comments

--- src/copy.ts
+++ src/copy.ts
@@ -9,7 +9,9 @@ export async function copy(
   // 因此这个检查的保障是偏弱的, 若在检查完毕后立即创建一个目录, 这项检查就会失效.
   if (await pathExists(destination)) throw new Error(`${destination} already exists`)
 
-  // 主流文件系统并不支持原子化复制目录, 因此"复制"这一行为本身就没什么保障可言.
+  // 主流文件系统并不支持原子化复制目录, 因此"复制"这一行为本身不具有保障性.
+  // 一种解决方案是在目标文件系统中建立一个临时目录, 然后将该临时目录移动至destination路径.
+  // `fs-extra`的`copy()`所具有的特性决定了它无法采用这种方案, 阅读源代码后, 也证实了这一点.
   await fs.copy(source, destination, {
     overwrite: false
   , errorOnExist: true
