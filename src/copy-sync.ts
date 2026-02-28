795b81c3358eebed5816e3ee1128f508ea3cf726
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2026-02-28 10:39:48 +0000

    docs: add comments

--- src/copy-sync.ts
+++ src/copy-sync.ts
@@ -1,6 +1,7 @@
 import fs from 'fs-extra'
 import { pathExistsSync } from './path-exists-sync.js'
 
+// 相关注释见`copy()`.
 export function copySync(source: string, destination: string): void {
   if (pathExistsSync(destination)) throw new Error(`${destination} already exists`)
 
--- src/copy.ts
+++ src/copy.ts
@@ -5,8 +5,11 @@ export async function copy(
   source: string
 , destination: string
 ): Promise<void> {
+  // `fs-extra`的`copy()`会在destination为目录时将source的内容移动至destination,
+  // 因此这个检查的保障是偏弱的, 若在检查完毕后立即创建一个目录, 这项检查就会失效.
   if (await pathExists(destination)) throw new Error(`${destination} already exists`)
 
+  // 主流文件系统并不支持原子化复制目录, 因此"复制"这一行为本身就没什么保障可言.
   await fs.copy(source, destination, {
     overwrite: false
   , errorOnExist: true
